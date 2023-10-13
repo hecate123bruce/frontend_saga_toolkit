3092a3ecf5982ec02aed0cf477b6962f05fd16a3
hecate123bruce <bruce.174553@gmail.com>
2023-10-13 11:34:05 +0300

    frist commit to manage User.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import {
   userActions,
